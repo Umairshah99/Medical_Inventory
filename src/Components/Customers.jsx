7a7ae62238f4aed4e5a37b3de65ca2b42c5b0742
AyeshaZfr <az1866@nyu.edu>
2022-12-28 00:02:38 +0500

    front-end

--- src/Components/Customers.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-export default class Customers extends Component  {
-    return (
-
-    );
-  }
-  
--- src/Components/Purchases.jsx
+++
@@ -1,8 +0,0 @@
-import React, { Component } from "react";
-
-export default class Purchases extends Component  {
-    return (
-
-    );
-  }
-  
